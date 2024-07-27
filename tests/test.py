cec7c08e91867f23390f04235a83920c0309411a
AbelolDev <aaravenaortiz4@gmail.com>
2024-07-27 18:42:39 -0400

    Función filtrar_datos_tabla

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,8 @@
-from libreria.modulo1 import *
+from libreria.modulo1 import db
 
 mi_db = db('Testing.db')
 
 # Uso de la clase db
+#mi_db.consultar_datos_tabla('jugadores')
+mi_db.filtrar_datos_tabla("jugadores", "creditos", "> 10")
+mi_db.cerrar_conexion()
